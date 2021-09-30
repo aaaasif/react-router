7cc07efb2544370bcf24b391672e3c4bc824f1f4
aaaasif <alexasif4@gmail.com>
2021-09-30 15:01:58 +0600

    js added

---
+++ src/components/Friend/Friend.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Friend = () => {
+    return (
+        <div>
+            <h1>this is single friend list</h1>
+        </div>
+    );
+};
+
+export default Friend;
