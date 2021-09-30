21bff997a0207c110d99b27bdcbe3b673f2d49a6
aaaasif <alexasif4@gmail.com>
2021-09-30 17:01:44 +0600

    js added

--- src/components/PostDetail/PostDetail.js
+++ src/components/PostDetail/PostDetail.js
@@ -4,12 +4,18 @@ import { useParams } from 'react-router';
 const PostDetail = () => {
     const { postId } = useParams();
     const [post, setPost] = useState({})
-    useEffect(() => {
-        const url = `https://jsonplaceholder.typicode.com/posts/${postId}`;
+    useEffect(() =>{
+        const url =`https://jsonplaceholder.typicode.com/posts/${postId}`;
         fetch(url)
-            .then(res => res.json())
-            .then(data => setPost(data))
-    }, [])
+              .then(res => res.json())
+              .then(data => setPost(data))
+    },[])
+    // useEffect(() => {
+    //     const url = `https://jsonplaceholder.typicode.com/posts/${postId}`;
+    //     fetch(url)
+    //         .then(res => res.json())
+    //         .then(data => setPost(data))
+    // }, [])
     return (
         <div>
             <h2>Post Detail: {postId}</h2>
