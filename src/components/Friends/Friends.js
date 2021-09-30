ed670ae7da8768fa43a87b669e9e079fd875b9d8
aaaasif <alexasif4@gmail.com>
2021-09-30 16:25:16 +0600

    js added

--- src/components/Friends/Friends.js
+++ src/components/Friends/Friends.js
@@ -1,15 +1,25 @@
 import React, { useEffect, useState } from 'react';
+import Friend from '../Friend/Friend';
+import './Friends.css';
 
 const Friends = () => {
-    const [friends, setfriends] = useState([]);
-    useEffect(() =>{
+    const [friends, setFriends] = useState([]);
+    useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
-        .then(res=> res.json())
-        .then(data => setfriends(data));
+            .then(res => res.json())
+            .then(data => setFriends(data));
     }, [])
     return (
         <div>
-            <h1>I have {friends.length} friends.</h1>
+            <h2>I have Friends: {friends.length}</h2>
+            <div className="friend-container">
+                {
+                    friends.map(friend => <Friend
+                        key={friend.id}
+                        friend={friend}
+                    ></Friend>)
+                }
+            </div>
         </div>
     );
 };
