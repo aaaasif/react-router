59db4985c9e800044ad7238b3a5643fa07e97b09
aaaasif <alexasif4@gmail.com>
2021-09-30 16:24:06 +0600

    js added

--- src/App.js
+++ src/App.js
@@ -1,33 +1,48 @@
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import logo from './logo.svg';
 import './App.css';
-import About from './components/About/About';
-import Friends from './components/Friends/Friends';
 import Home from './components/Home/Home';
+import Friends from './components/Friends/Friends';
+import About from './components/About/About';
 import NotFound from './components/NotFound/NotFound';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Header from './components/Header/Header';
+import FriendDetail from './components/FriendDetail/FriendDetail';
+import Culture from './components/Culture/Culture';
+import PostDetail from './components/PostDetail/PostDetail';
 
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <Router>
+        <Header></Header>
         <Switch>
-          <Route path='/home'>
+          <Route exact path="/">
             <Home></Home>
           </Route>
-          <Route path='/about'>
-            <About></About>
+          <Route path="/home">
+            <Home></Home>
           </Route>
-          <Route path='/friends'>
+          <Route path="/friends">
             <Friends></Friends>
           </Route>
-          <Route exact path='/'>
-            <Home></Home>
+          <Route path="/friend/:friendId">
+            <FriendDetail></FriendDetail>
           </Route>
-          <Route path='/'>
+          <Route path="/post/:postId">
+            <PostDetail></PostDetail>
+          </Route>
+          <Route exact path="/about">
+            <About></About>
+          </Route>
+          <Route exact path="/about/culture">
+            <Culture></Culture>
+          </Route>
+          <Route path="*">
             <NotFound></NotFound>
           </Route>
         </Switch>
-      </BrowserRouter>
+      </Router>
     </div>
   );
 }
--- src/components/Friend/Friend.js
+++ src/components/Friend/Friend.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import { Link, useHistory } from 'react-router-dom';
 
-const Friend = () => {
+const Friend = (props) => {
+    const { id, name, phone, website, address } = props.friend;
+    const history = useHistory();
+
+    const friendStyle = {
+        border: '3px solid goldenrod',
+        padding: '10px',
+        borderRadius: '10px'
+    };
+    const url = `/friend/${id}`;
+
+    const handleFriendClick = () => {
+        history.push(`/friend/${id}`);
+    }
+
     return (
-        <div>
-            <h1>this is single friend list</h1>
+        <div style={friendStyle}>
+            <h2>I am: {name} {id}</h2>
+            <h5>Call me: {phone}</h5>
+            <p>Visit me: {website}</p>
+            <p><small>I live in: {address.city}</small></p>
+            <Link to={url}>Visit me</Link >
+            <br />
+            <Link to={url}>
+                <button>Visit Me</button>
+            </Link>
+            <br />
+            <button onClick={handleFriendClick}>Visit me 2</button>
         </div>
     );
 };
