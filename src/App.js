3bca5ab98e386bbfa619510de29e9577fe44f64a
aaaasif <alexasif4@gmail.com>
2021-09-30 11:16:57 +0600

    js added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
-import Home from './components/Home/Home';
+
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      
     </div>
   );
 }
-
 export default App;
