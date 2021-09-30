48ed7c773d05287e9d87771380accb43a41c5f8b
aaaasif <alexasif4@gmail.com>
2021-09-30 11:38:59 +0600

    js added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
+import About from './components/About/About';
+import Friends from './components/Friends/Friends';
 import Home from './components/Home/Home';
+import NotFound from './components/NotFound/NotFound';
 
 
 function App() {
   return (
     <div className="App">
       <Home/>
-      
+      <About/>
+      <Friends/>
+      <NotFound/>
     </div>
   );
 }
